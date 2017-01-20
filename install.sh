11be578355a938801673ae2d75bea98fd5651dc7
Nigel Rahkola <me@nigelrahkola.com>
2017-01-19 22:50:00 -0800

    Create symlinks of some files

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ echo "Installing dotfiles"
 
 echo "Found the following dotfiles"
 dotfiles_dir="$HOME/dev/.dotfiles"
-for entry in "$dotfiles_dir"/*
+for entry in $dotfiles_dir/*
 do
-  echo "$entry"
+  echo "Linking $entry"
+  ln -s $entry "$PWD/tst"
 done
