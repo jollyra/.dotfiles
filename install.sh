346ea1968d58c32627756cb9d1983dbdf53a165e
Nigel Rahkola <me@nigelrahkola.com>
2017-01-20 21:08:58 -0800

    Set git user in install script

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@ echo "Installing dotfiles"
 
 echo "Found the following dotfiles"
 dotfiles_dir="$HOME/dev/.dotfiles"
-for entry in $dotfiles_dir/*
-do
-  echo "Linking $entry"
+for file in `ls -A $dotfiles_dir/*`; do
+  echo "Linking $file"
   ln -s $entry "$PWD/tst"
 done
+
+# Set git user
+[ -z `git config --global user.name` ] && git config --global user.name "Nigel"
+[ -z `git config --global user.email` ] && git config --global user.email "me@nigelrahkola.com"
